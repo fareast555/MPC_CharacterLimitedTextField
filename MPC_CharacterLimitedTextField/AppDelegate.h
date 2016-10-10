0f48984c0dd48aaf73539e1ab826a5fa79f49a77
Mike <critchley55@yahoo.co.jp>
2016-10-10 23:23:24 +0700

    Initial Commit

---
+++ MPC_CharacterLimitedTextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MPC_CharacterLimitedTextField
+//
+//  Created by Michael Critchley on 10/10/16.
+//  Copyright © 2016 Michael Critchley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
